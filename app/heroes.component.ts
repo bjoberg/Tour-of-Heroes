1981f104baf9413b47011c6f5f5ac9247bcf94ea
Brett Oberg <brett@obergmail.com>
2016-09-10 12:50:34 +0800

    HTTP > Search-by-name

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -14,17 +14,42 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
   constructor(private router: Router, private heroService: HeroService) { }
+
   getHeroes(): void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
+
   ngOnInit(): void {
     this.getHeroes();
   }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+
   gotoDetail(): void {
     let link = ['/detail', this.selectedHero.id];
     this.router.navigate(link);
   }
+
+  add(name: string): void {
+    name = name.trim();
+    if(!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+          this.heroes.push(hero);
+          this.selectedHero = null;
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroService
+      .delete(hero.id)
+      .then(() => {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        if (this.selectedHero === hero) { this.selectedHero = null; }
+      });
+  }
+
+
 }
